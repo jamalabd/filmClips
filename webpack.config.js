66506e41f11f3de050c2e64af7f571479e00aa0b
Jamal <jamalabdul47@gmail.com>
2019-07-03 01:43:30 -0400

    setting upweb pack

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  // entry point
+  entry: './src/controller/index.js',
+
+  // output point
+  output: {
+    path: __dirname + 'dist',
+    filename:'bundle.js'
+  },
+  watch: true,
+  module: {
+    rules:[
+      {test:/\.js$/, exclude: /(node_modules)/, loader:'babel-loader', query:{presets: ['@babel/preset-env']}},
+      {test:/\.css$/, loader: 'style-loader!css-loader'}
+    ]
+  }
+};
